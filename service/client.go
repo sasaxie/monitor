6335bf48fb85e0d16f0e8548baa13a9fdd571a91
sasaxie <xiexiaodong917@163.com>
2018-06-26 16:03:24 +0800

    refactoring code

--- service/client.go
+++ service/client.go
@@ -3,12 +3,16 @@ package service
 import (
 	"context"
 	"github.com/sasaxie/monitor/api"
-	"github.com/sasaxie/monitor/common/base58"
 	"github.com/sasaxie/monitor/common/hexutil"
-	"github.com/sasaxie/monitor/core"
 	"google.golang.org/grpc"
 	"log"
+	"github.com/sasaxie/monitor/util"
+	"github.com/sasaxie/monitor/models"
+	"sync"
+	"time"
 )
+
+const GrpcTimeout = 5 * time.Second
 
 type GrpcClient struct {
 	Address        string
@@ -34,66 +38,20 @@ func (g *GrpcClient) Start() {
 	g.DatabaseClient = api.NewDatabaseClient(g.Conn)
 }
 
-func (g *GrpcClient) ListWitnesses() *api.WitnessList {
-	witnessList, err := g.WalletClient.ListWitnesses(context.Background(),
-		new(api.EmptyMessage))
+func (g *GrpcClient) GetNowBlock(block *models.Block, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	if err != nil {
-		log.Fatalf("get witnesses error: %v\n", err)
-	}
-
-	return witnessList
-}
-
-func (g *GrpcClient) ListNodes() *api.NodeList {
-	nodeList, err := g.WalletClient.ListNodes(context.Background(),
-		new(api.EmptyMessage))
-
-	if err != nil {
-		log.Fatalf("get nodes error: %v\n", err)
-	}
-
-	return nodeList
-}
-
-func (g *GrpcClient) GetAccount(address string) *core.Account {
-	account := new(core.Account)
-
-	account.Address = base58.DecodeCheck(address)
-
-	result, err := g.WalletClient.GetAccount(context.Background(), account)
-
-	if err != nil {
-		log.Fatalf("get account error: %v\n", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetNowBlock() *core.Block {
-	result, err := g.WalletClient.GetNowBlock(context.Background(), new(api.EmptyMessage))
+	ctx, cancel := context.WithTimeout(context.Background(), GrpcTimeout)
+	defer cancel()
+	result, err := g.WalletClient.GetNowBlock(ctx, new(api.EmptyMessage))
 
 	if err != nil {
 		log.Printf("get now block error: %v\n", err)
-		return nil
+		return
 	}
 
-	return result
-}
-
-func (g *GrpcClient) GetAssetIssueByAccount(address string) *api.AssetIssueList {
-	account := new(core.Account)
-
-	account.Address = base58.DecodeCheck(address)
-
-	result, err := g.WalletClient.GetAssetIssueByAccount(context.Background(),
-		account)
-
-	if err != nil {
-		log.Fatalf("get asset issue by account error: %v", err)
-	}
-
-	return result
+	block.Hash = hexutil.Encode(util.GetBlockHash(*result))
+	block.Number = result.GetBlockHeader().GetRawData().GetNumber()
 }
 
 func (g *GrpcClient) GetNextMaintenanceTime() *api.NumberMessage {
@@ -120,131 +78,19 @@ func (g *GrpcClient) TotalTransaction() *api.NumberMessage {
 	return result
 }
 
-func (g *GrpcClient) GetAccountNet(address string) *api.AccountNetMessage {
-	account := new(core.Account)
+func (g *GrpcClient) GetLastSolidityBlockNum(result *models.Result,
+	wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	account.Address = base58.DecodeCheck(address)
+	ctx, cancel := context.WithTimeout(context.Background(), GrpcTimeout)
+	defer cancel()
 
-	result, err := g.WalletClient.GetAccountNet(context.Background(), account)
-
-	if err != nil {
-		log.Fatalf("get account net error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetAssetIssueByName(name string) *core.AssetIssueContract {
-
-	assetName := new(api.BytesMessage)
-	assetName.Value = []byte(name)
-
-	result, err := g.WalletClient.GetAssetIssueByName(context.Background(), assetName)
-
-	if err != nil {
-		log.Fatalf("get asset issue by name error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetBlockByNum(num int64) *core.Block {
-	numMessage := new(api.NumberMessage)
-	numMessage.Num = num
-
-	result, err := g.WalletClient.GetBlockByNum(context.Background(), numMessage)
-
-	if err != nil {
-		log.Fatalf("get block by num error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetBlockById(id string) *core.Block {
-	blockId := new(api.BytesMessage)
-	var err error
-
-	blockId.Value, err = hexutil.Decode(id)
-
-	if err != nil {
-		log.Fatalf("get block by id error: %v", err)
-	}
-
-	result, err := g.WalletClient.GetBlockById(context.Background(), blockId)
-
-	if err != nil {
-		log.Fatalf("get block by id error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetAssetIssueList() *api.AssetIssueList {
-
-	result, err := g.WalletClient.GetAssetIssueList(context.Background(), new(api.EmptyMessage))
-
-	if err != nil {
-		log.Fatalf("get asset issue list error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetBlockByLimitNext(start, end int64) *api.BlockList {
-	blockLimit := new(api.BlockLimit)
-	blockLimit.StartNum = start
-	blockLimit.EndNum = end
-
-	result, err := g.WalletClient.GetBlockByLimitNext(context.Background(), blockLimit)
-
-	if err != nil {
-		log.Fatalf("get block by limit next error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetTransactionById(id string) *core.Transaction {
-	transactionId := new(api.BytesMessage)
-	var err error
-
-	transactionId.Value, err = hexutil.Decode(id)
-
-	if err != nil {
-		log.Fatalf("get transaction by id error: %v", err)
-	}
-
-	result, err := g.WalletClient.GetTransactionById(context.Background(), transactionId)
-
-	if err != nil {
-		log.Fatalf("get transaction by limit next error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetBlockByLatestNum(num int64) *api.BlockList {
-	numMessage := new(api.NumberMessage)
-	numMessage.Num = num
-
-	result, err := g.WalletClient.GetBlockByLatestNum(context.Background(), numMessage)
-
-	if err != nil {
-		log.Fatalf("get block by latest num error: %v", err)
-	}
-
-	return result
-}
-
-func (g *GrpcClient) GetLastSolidityBlockNum() int64 {
-	dynamicProperties, err := g.DatabaseClient.GetDynamicProperties(context.
-		Background(), new(api.EmptyMessage))
+	dynamicProperties, err := g.DatabaseClient.GetDynamicProperties(ctx, new(api.EmptyMessage))
 
 	if err != nil {
 		log.Printf("get last solidity block num error: %v", err)
-		return 0
+		return
 	}
 
-	return dynamicProperties.LastSolidityBlockNum
+	result.LastSolidityBlockNum = dynamicProperties.LastSolidityBlockNum
 }
