36ab8b526b59db0960a18a077191e0b0c234f6a8
sasaxie <xiexiaodong917@163.com>
2018-12-29 14:14:11 +0800

    :wrench:modify report schedule

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func report() {
 	c.AddFunc("0 0 11 * * *", func() {
 		logs.Debug("report start")
 		r := new(reports.TotalMissed)
-		r.Date = time.Now().AddDate(0, 0, 0)
+		r.Date = time.Now().AddDate(0, 0, -1)
 		logs.Debug("report date", r.Date.Format("2006-01-02 15:04:05"))
 		r.ComputeData()
 		r.Save()
